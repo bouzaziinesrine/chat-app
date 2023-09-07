fa016054314500735bf483c5d89a284af99291f5
bouzaziinesrine <bouzazinesrine10@gmail.com>
2023-09-07 11:48:22 +0100

    firebase config changed

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -10,12 +10,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDFDYLM0irAxbAgLBhzkbQ1a8KdQQaTI5s",
-  authDomain: "chat-app-dc04e.firebaseapp.com",
-  projectId: "chat-app-dc04e",
-  storageBucket: "chat-app-dc04e.appspot.com",
-  messagingSenderId: "11532195052",
-  appId: "1:11532195052:web:dd10951d5b503709e741c9"
+  apiKey: "AIzaSyDz89fCCYHfprALTd79EFz2yAk9c6K-0sA",
+  authDomain: "chat-app-34856.firebaseapp.com",
+  projectId: "chat-app-34856",
+  storageBucket: "chat-app-34856.appspot.com",
+  messagingSenderId: "830230878270",
+  appId: "1:830230878270:web:fe7af8e234ca45c821996f"
 };
 
 // Initialize Firebase
